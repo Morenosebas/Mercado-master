590f51d416b3ac85a7552f04550a49a827a5b1a9
Morenosebas <johasegom08@gmail.com>
2023-04-11 23:43:29 -0500

    dsfsdf

--- src/Components/publicComponents/shops.jsx
+++ src/Components/publicComponents/shops.jsx
@@ -2,16 +2,16 @@
 export const ShopPublicList = ({ shopList }) => {
 
 
-    return <div className="container d-flex   align-items-center justify-content-center position-relative"
+    return <div className="container d-flex align-items-start justify-content-center position-relative"
         style={
             {
-                top: 100, width: "100%", zIndex: -1, flexWrap: "wrap"
+                top: 80, width: "100%", zIndex: -1, flexWrap: "wrap",
             }
         }
     >
         {
             shopList.map(element => (
-                <div className="card" style={{ width: "200px", height: "200px", margin: "2px" }}>
+                <div className="card styleScroll" style={{ width: "200px", height: "320px", marginLeft: "30px", marginBottom: "30px",overflow:"auto" }}>
                     <div className="card-header d-flex">{element.nombre}    </div>
                     <img src={require("../img/FastEats.png")} alt="logo imagen" className="card-img-top w-20" />
                     <div className="card-body">
